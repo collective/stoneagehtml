4db2b3c9139d88e808747e1087cfaa0d3ba6da1b
Malthe Borch <mborch@gmail.com>
2020-01-25 18:32:48 +0100

    Add Python 3.8 to support versions

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Internet",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Text Processing :: Markup :: HTML",
